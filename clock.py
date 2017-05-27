28f2d1528e24bb98425f613a3ca06e71d3eea866
brandonpwood <bwood0918@bths.edu>
2017-05-27 12:54:33 -0400

    changed time

--- clock.py
+++ clock.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler as bs
 import bin as bins
 sched = bs()
 
-@sched.scheduled_job('interval', minutes = 10)
+@sched.scheduled_job('interval', minutes = 3)
 def run():
     bins.bot.login_bot()
     bisn.bot.run_bot()
